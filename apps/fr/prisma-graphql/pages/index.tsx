462fe6533a2dd917e68053abb1ce607291f6b652
MHMoradian <mohammadhmoradian@gmail.com>
2023-12-03 20:21:21 +0330

    :hammer: created prisma-graphql front app

---
+++ apps/fr/prisma-graphql/pages/index.tsx
@@ -0,0 +1,18 @@
+import { TestGraph } from 'fr/fr-shared';
+import { getCountriesByGraphMethods } from 'fr/fr-states';
+
+export function Index() {
+  return <TestGraph />;
+}
+
+export default Index;
+
+export const getServerSideProps = async () => {
+  const countries = await getCountriesByGraphMethods(1, 10);
+
+  return {
+    props: {
+      initialZustandState: { countries },
+    },
+  };
+};
--- libs/fr/fr-shared/src/lib/components/pages/index.ts
+++ libs/fr/fr-shared/src/lib/components/pages/index.ts
@@ -1,1 +1,2 @@
 export * from './TestRest';
+export * from './TestGraph';
