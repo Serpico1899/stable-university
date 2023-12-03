c391b59b90ca9c6ffc93af63da97241640a661ad
emirom <meyari73@gmail.com>
2023-12-03 16:52:56 +0330

    feat: add get many

--- apps/bc/prisma-postgres/src/services/country/getManyCountries.service.ts
+++ apps/bc/prisma-postgres/src/services/country/getManyCountries.service.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const getManyCountries = async ( _limit=10, skip=0) => {
+const getManyCountries = async ( limit=10, skip=0) => {
 	return await prisma.country.findMany({
-		take:10,
+		take:limit,
 		skip
 	});	
 }
